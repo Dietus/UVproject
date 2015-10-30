8a17b6ff60c3f9df80330600846b6be513453a7e
Dietus <luis.iau.ribeiro@gmail.com>
2015-10-30 22:20:19 +0000

    restructuring to form a class

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,30 @@
 fitzpatrick = {'I': 87.6, 'II': 109.4, 'III': 131.3, 'IV': 197.0, 'V': 262.8, 'VI': 437.8}
 
 
-def get_time():
+class Skin(object):
+    def __init__(self, skin_type, skin_exposure, uv_index):
+        self.skin_type = skin_type
+        self.skin_exposure = skin_exposure
+        self.uv_index = uv_index
+
+    def time_needed(self):
+        return int((self.skin_type/(self.uv_index/40))/(self.skin_exposure/25))
+
+    def get_time(self):
+        time = int((self.skin_type/(self.uv_index/40))/(self.skin_exposure/25))
+        seconds_needed = time % 60
+        minutes_needed = (time % 3600)/60
+        hours_needed = time/3600
+        print 'You need %s hours %s minutes and %s seconds of sunlight every other day to get your recommended Vit D dose.' % (hours_needed, minutes_needed, seconds_needed)
+
+
+def calculate_uv():
     skin_type = fitzpatrick[raw_input('What is your Fitzpatrick skin type (I-VI)?').upper()]
     skin_exposure = float(raw_input('What % of your skin is exposed?'))
     uv_index = float(raw_input('What is the current UV index of your city?'))
 
-    time_needed = int((skin_type/(uv_index/40))/(skin_exposure/25))
-    seconds_needed = time_needed % 60
-    minutes_needed = (time_needed % 3600)/60
-    hours_needed = time_needed/3600
-    print 'You need %s hours %s minutes and %s seconds of sunlight every other day to get your recommended Vit D dose.' % (hours_needed, minutes_needed, seconds_needed)
+    user = Skin(skin_type, skin_exposure, uv_index)
 
+    user.get_time()
 
-get_time()
+calculate_uv()
