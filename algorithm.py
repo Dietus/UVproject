3620974a0860f1e2640a2a8d4e613e93226b23b6
Dietus <luis.iau.ribeiro@gmail.com>
2015-11-01 11:06:01 +0000

    Updated skin_exposure choices, also added exceptions to 0 skin exposure to prevent dividing by 0

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,6 @@
 fitzpatrick = {'I': 87.6, 'II': 109.4, 'III': 131.3, 'IV': 197.0, 'V': 262.8, 'VI': 437.8}
+skin_area = {'Face': 4, 'Neck': 2, 'Chest': 26, 'Hands': 6, 'Arms': 14, 'Legs': 14, 'Thighs': 18}
+answers = ['Y', 'YES', 'N', 'NO']
 
 
 class Skin(object):
@@ -8,18 +10,24 @@ class Skin(object):
         self.uv_index = uv_index
 
     def get_time(self):  # used to calculate time in sunlight needed to get SDD (Standard Vit D Dose)
-        time = int((self.skin_type / (self.uv_index / 40)) / (self.skin_exposure / 25))
-        seconds_needed = time % 60
-        minutes_needed = (time % 3600) / 60
-        hours_needed = time / 3600
-        print 'You need %s hours %s minutes and %s seconds of sunlight every other day to get your recommended Vit D dose.' % (hours_needed, minutes_needed, seconds_needed)
+        if self.skin_exposure == 0:
+            print 'You need some skin exposure to get Vit D from the sun.'
+        else:
+            time = int((self.skin_type / (self.uv_index / 40)) / (self.skin_exposure / 25))
+            seconds_needed = time % 60
+            minutes_needed = (time % 3600) / 60
+            hours_needed = time / 3600
+            print 'You need %s hours %s minutes and %s seconds of sunlight every other day to get your recommended Vit D dose.' % (hours_needed, minutes_needed, seconds_needed)
 
-    def get_burn(self): # used to calculate MED (minimal erythema dose)
-        time = int(4 * (self.skin_type / (self.uv_index / 40)))
-        seconds_needed = time % 60
-        minutes_needed = (time % 3600) / 60
-        hours_needed = time / 3600
-        print 'You need a minimum %s hours %s minutes and %s seconds of sunlight to burn.' % (hours_needed, minutes_needed, seconds_needed)
+    def get_burn(self):  # used to calculate MED (minimal erythema dose)
+        if self.skin_exposure == 0:
+            print 'You need to have skin exposed to be burnt'
+        else:
+            time = int(4 * (self.skin_type / (self.uv_index / 40)))
+            seconds_needed = time % 60
+            minutes_needed = (time % 3600) / 60
+            hours_needed = time / 3600
+            print 'You need a minimum %s hours %s minutes and %s seconds of sunlight to burn.' % (hours_needed, minutes_needed, seconds_needed)
 
 
 def calculate_uv():
@@ -28,20 +36,13 @@ def calculate_uv():
         raw_type = raw_input('Answer not valid. What is your Fitzpatrick skin type (I-VI)? ').upper()
     skin_type = fitzpatrick[raw_type]
 
-    exposure = False
-    while not exposure:
-        raw_exposure = raw_input('What % of your skin is exposed? ')
-        for i in raw_exposure:
-            if i == '%':
-                raw_exposure = raw_exposure[:len(raw_exposure) - 1]
-        try:
-            skin_exposure = float(raw_exposure)
-            exposure = True
-        except ValueError:
-            print 'Answer not valid. Please retry.'
-
-    while skin_exposure >= 100:
-        skin_exposure = float(raw_input('Answer not valid. What % of your skin is exposed? '))
+    skin_exposure = 0
+    for i in skin_area:
+        area = raw_input(i + ' exposed?(Y/N) ').upper()
+        while area not in answers:
+            area = raw_input('Answer not valid. ' + i + ' exposed?(Y/N) ').upper()
+        if area == 'Y' or area == 'YES':
+            skin_exposure += skin_area[i]
 
     uv = False
     while not uv:
